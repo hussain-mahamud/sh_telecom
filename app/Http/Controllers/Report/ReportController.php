41e2ea6c17cabeca6eeecf07217264cf5f040ec9
Hussain Mahamud <hussainmahamud.swe@gmail.com>
2021-07-11 16:02:22 +0600

    Adding Carbon

--- app/Http/Controllers/Report/ReportController.php
+++ app/Http/Controllers/Report/ReportController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Purchase\Purchase;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 class ReportController extends Controller
 {
     public function index(){
