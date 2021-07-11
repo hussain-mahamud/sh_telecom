4a47fdee3e9c6ec79a504fab013c03b2ff479b72
Hussain Mahamud <hussainmahamud.swe@gmail.com>
2021-07-11 14:59:18 +0600

    curdateChanging check

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,10 +10,10 @@ class AdminController extends Controller
 {
     public function index(){
     	$daily_sales=DB::table('purchases')
-    	    			->whereRaw('Date(created_at) = CURDATE()')
+    	    			->whereRaw('Date(created_at) = RETURN current_date')
     	    			->sum('pr_price');
     	$weekly_sales=DB::table('purchases')
-    	    			->whereRaw('Date(purchases.created_at) >= DATE_SUB(CURDATE(), INTERVAL 6 DAY)')
+    	    			->whereRaw('Date(purchases.created_at) >= DATE_SUB(RETURN current_date, INTERVAL 6 DAY)')
     	    			->sum('pr_price');
     	return view('admin.index',['daily_sales'=>$daily_sales,'weekly_sales'=>$weekly_sales]);
     }
