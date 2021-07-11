0adb881e7d451a6472c6b7f6f1a79cea7ec0ea1b
Hussain Mahamud <hussainmahamud.swe@gmail.com>
2021-07-11 15:38:57 +0600

    curdateChanging check

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Purchase\Purchase;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 class AdminController extends Controller
 {
     public function index(){
@@ -16,9 +17,12 @@ class AdminController extends Controller
     	// $daily_sales=DB::table('purchases')
     	//     			->whereRaw('Date(created_at) = CURDATE()')
     	//     			->sum('pr_price');
-    	// $weekly_sales=DB::table('purchases')
-    	//     			->whereRaw('Date(purchases.created_at) >= DATE_SUB(CURDATE(), INTERVAL 6 DAY)')
-    	//     			->sum('pr_price');
+    	$weekly_sales=DB::table('purchases')
+						->select('pr_price')
+						->whereBetween('created_at', [
+							Carbon::parse('last saturday')->startOfDay(),
+							Carbon::parse('next friday')->endOfDay(),
+						])->sum('pr_price');
     	return view('admin.index',['daily_sales'=>$daily_sales,'weekly_sales'=>$weekly_sales]);
     }
 }
